882d8f8363927c4be057d59a3303f66b65b5d6eb
do noi <ntdbna1@gmail.com>
2020-08-28 00:35:13 +0700

    update

--- src/components/Staff/Notification.jsx
+++
@@ -1,9 +0,0 @@
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-toast.configure();
-export default Notification = (content, type, close) => {
-  return toast(content, {
-    type: "" + type,
-    autoClose: close,
-  });
-};
